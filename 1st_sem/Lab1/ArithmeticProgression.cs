74a9c70d81cfc456c85b0c76c0119640282b28fc
Dmytro Kyba <dkyba2006@gmail.com>
2024-10-03 10:59:44 +0300

    added try catch exception to indexator

--- 1st_sem/Lab1/ArithmeticProgression.cs
+++ 1st_sem/Lab1/ArithmeticProgression.cs
@@ -1,93 +1,107 @@
 using System.Text;
 
-namespace Lab1 {
-class ArithmeticProgression
+namespace Lab1
 {
-  private double firstElement;
-  private double difference;
-  private double amount;
-
-  public double FirstElement
+  class ArithmeticProgression
   {
-    get { return firstElement; }
-    set { firstElement = value; }
-  }
+    private double firstElement;
+    private double difference;
+    private double amount;
 
-  public double Difference
-  {
-    get { return difference; }
-    set { difference = value; }
-  }
-
-  public double Amount
-  {
-    get { return amount; }
-    set { amount = value; }
-  }
-
-  public ArithmeticProgression(double firstElement, double difference, double amount)
-  {
-    this.firstElement = firstElement;
-    this.difference = difference;
-    this.amount = amount;
-  }
-
-  public ArithmeticProgression()
-  {
-    this.firstElement = 0;
-    this.difference = 0;
-    this.amount = 0;
-  }
-
-  public double this[int index]
-  {
-    get
+    public double FirstElement
     {
-      return firstElement + index * difference;
+      get { return firstElement; }
+      set { firstElement = value; }
     }
-  }
 
-  public override string ToString()
-{
-    StringBuilder progression = new StringBuilder();
-
-    for (int i = 0; i < amount; i++)
+    public double Difference
     {
+      get { return difference; }
+      set { difference = value; }
+    }
+
+    public double Amount
+    {
+      get { return amount; }
+      set { amount = value; }
+    }
+
+    public ArithmeticProgression(double firstElement, double difference, double amount)
+    {
+      this.firstElement = firstElement;
+      this.difference = difference;
+      this.amount = amount;
+    }
+
+    public ArithmeticProgression()
+    {
+      this.firstElement = 0;
+      this.difference = 0;
+      this.amount = 0;
+    }
+
+    public double this[int index]
+    {
+      get
+      {
+        try
+        {
+          if (index < 0 || index >= amount)
+          {
+            throw new ArgumentOutOfRangeException(nameof(index), "Index is out of range, but custom exception");
+          }
+
+          return firstElement + index * difference;
+        }
+        catch (ArgumentOutOfRangeException ex)
+        {
+          Console.WriteLine($"Error: {ex.Message}");
+          throw;
+        }
+      }
+    }
+
+
+    public override string ToString()
+    {
+      StringBuilder progression = new StringBuilder();
+
+      for (int i = 0; i < amount; i++)
+      {
         progression.Append(this[i]);
 
         if (i < amount - 1)
         {
-            progression.Append(", ");
+          progression.Append(", ");
         }
+      }
+
+      return $"Arithmetic Progression: {progression}";
     }
 
-    return $"Arithmetic Progression: {progression}";
-}
+
+    public void Input()
+    {
+      Console.WriteLine("Enter the first element of the arithmetic progression:");
+      FirstElement = double.Parse(Console.ReadLine());
+
+      Console.WriteLine("Enter the difference:");
+      Difference = double.Parse(Console.ReadLine());
+
+      Console.WriteLine("Enter the amount of elements:");
+      Amount = double.Parse(Console.ReadLine());
+    }
+
+    public void Output()
+    {
+      Console.WriteLine(ToString());
+    }
 
 
-  public void Input()
-  {
-    Console.WriteLine("Enter the first element of the arithmetic progression:");
-    FirstElement = double.Parse(Console.ReadLine());
+    public double SumOfSequence()
+    {
+      return (amount * (2 * firstElement + (amount - 1) * difference)) / 2;
+    }
 
-    Console.WriteLine("Enter the difference:");
-    Difference = double.Parse(Console.ReadLine());
-
-    Console.WriteLine("Enter the amount of elements:");
-    Amount = double.Parse(Console.ReadLine());
   }
-
-  public void Output()
-  {
-    Console.WriteLine(ToString());
-    Console.WriteLine(SumOfSequence());
-  }
-
-
-  public double SumOfSequence()
-  {
-    return (amount * (2 * firstElement + (amount - 1) * difference)) / 2;
-  }
-
-}
 }
--- 1st_sem/Lab1/Program.cs
+++ 1st_sem/Lab1/Program.cs
@@ -1,20 +1,19 @@
 ﻿using System;
 
-namespace Lab1{
-class Program
+namespace Lab1
 {
-  static void Main(string[] args)
+  class Program
   {
-    ArithmeticProgression ap1 = new ArithmeticProgression(1, 4, 5);
-    Console.WriteLine(ap1[3]);
-    ArithmeticProgression ap2 = new();
-    ap2.Input();
-    ap2.Output();
-    Console.WriteLine(ap2.SumOfSequence());
+    static void Main(string[] args)
+    {
+      ArithmeticProgression ap1 = new ArithmeticProgression(1.0, 4, 5);
+      Console.WriteLine(ap1[3]);
+      Console.WriteLine(ap1[7]);
+      ArithmeticProgression ap2 = new();
+      ap2.Input();
+      ap2.Output();
+      Console.WriteLine(ap2.SumOfSequence());
 
-    // Console.WriteLine(ap[2]);
-    // Console.WriteLine(ap);
-    // Console.WriteLine(ap.SumOfSequence());
+    }
   }
-}
 }
