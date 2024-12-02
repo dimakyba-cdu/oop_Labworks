66559aa45051e92ebdf9eef73f79a6e1db7479ab
Dmytro Kyba <dkyba2006@gmail.com>
2024-12-02 15:21:26 +0200

    added TestSquareDifference method and showed the work of comparator using sort

--- 1st_sem/interfaces_as_inheritance/Lab/obj/Debug/net8.0/interfaces_as_inheritance.AssemblyInfo.cs
+++ 1st_sem/interfaces_as_inheritance/Lab/obj/Debug/net8.0/interfaces_as_inheritance.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("interfaces_as_inheritance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c84a4069680c69aa96355c87ffe8be09de44615e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8f42f81af04e69a84f3e307a1b0f1e90c559d35")]
 [assembly: System.Reflection.AssemblyProductAttribute("interfaces_as_inheritance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("interfaces_as_inheritance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
