6f4842670f23935288efe7a91e18f66ebfffc4f1
Dmytro Kyba <dkyba2006@gmail.com>
2025-03-19 23:58:33 +0200

    Block2✅

--- 2nd_sem/Lab1_delegate/Block2/Program.cs
+++ 2nd_sem/Lab1_delegate/Block2/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+
+internal class Program
+{
+  delegate bool Filter(int n);
+
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Enter the array into single line with spaces within:");
+    // int[] arr = Array.ConvertAll(Console.ReadLine().Trim().Split(), Convert.ToInt32);
+    int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    Console.Write("Enter the divisor: ");
+    int k = Convert.ToInt32(Console.ReadLine());
+
+    Filter filterMultiple = x => x % k == 0;
+
+    Console.WriteLine($"LINQ: {string.Join(", ", FilterUsingLinq(arr, filterMultiple))}");
+    Console.WriteLine($"Manually: {string.Join(", ", FilterManually(arr, filterMultiple))}");
+    FilterUsingLinq(arr, filterMultiple);
+    FilterManually(arr, filterMultiple);
+  }
+
+  static int[] FilterUsingLinq(int[] arr, Filter filter)
+  {
+    return arr.Where((x) => filter(x)).ToArray();
+  }
+
+  static int[] FilterManually(int[] arr, Filter filter)
+  {
+    int length = 0;
+    foreach (int num in arr)
+      if (filter(num))
+        length++;
+
+    int[] filteredArr = new int[length];
+    int j = 0;
+    foreach (int num in arr)
+    {
+      if (filter(num))
+        filteredArr[j++] = num;
+    }
+
+    return filteredArr;
+  }
+
+}
